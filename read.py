2500b553f3719c4e97eb47cca4d91ffaabdbf4ef
Wei Sui <weekysui@hotmail.com>
2020-05-12 21:52:37 -0700

    Delete read.py

--- read.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-# connect the sqlitedb
-conn = sqlite3.connect('project.db')
-c = conn.cursor()
-
-serviceTable = c.execute('select * from services')
-for row in serviceTable:
-    print(row)
